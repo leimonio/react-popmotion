5e6d844b2399a181ba85006a5e637c7878b4373b
Konstantinos Leimonis <konstantinos.leimonis@persado.com>
2015-10-19 10:00:48 +0300

    add(PopmotionComponent): really simple implementation wrapping popmotion usage

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,25 @@ require('normalize.css');
 require('styles/App.css');
 
 import React from 'react/addons';
+import PopmotionComponent from './PopmotionComponent';
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <PopmotionComponent
+            actorSelector=".index"
+            actionType="Tween"
+            values={ {y: 100} }
+            duration={600}
+            delay={1000}
+            runOnMount={true}>
+          <div className="index">
+            <img src={yeomanImage} alt="Yeoman Generator" />
+            <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+          </div>
+        </PopmotionComponent>
     );
   }
 }
