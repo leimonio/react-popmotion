7012cc8626699560c88422317672bdf6ca8ac247
Konstantinos Leimonis <konstantinos.leimonis@persado.com>
2015-12-01 00:25:42 +0200

    update karma configuration for TravisCI build

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,25 @@
 var webpackCfg = require('./webpack.config');
 
+var isCI = process.env.CONTINUOUS_INTEGRATION === 'true';
+var runCoverage = process.env.COVERAGE === 'true' || isCI;
+var devBrowser = process.env.PHANTOM ? 'PhantomJS' : 'Chrome';
+
 module.exports = function(config) {
   config.set({
     basePath: '',
-    browsers: ['PhantomJS'],
+    browsers: [ isCI ? 'ChromeTravisCI' : devBrowser ],
+    customLaunchers: {
+      ChromeTravisCI: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     files: [
       'test/run.js'
     ],
     port: 8080,
     captureTimeout: 60000,
+    browserNoActivityTimeout: 45000,
     frameworks: ['phantomjs-shim', 'mocha', 'chai'],
     client: {
       mocha: {}
